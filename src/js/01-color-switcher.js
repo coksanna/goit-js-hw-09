21bbf05e2f35b37565fef042c66bdb2643b279df
Oksana Kyryliuk <coksanna@gmail.com>
2022-11-25 13:03:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+// console.log(stopBtn);
+const bodyRef = document.querySelector('body');
+// console.log(bodyRef);
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChange);
+function changeColor(e) {
+    startBtn.setAttribute('disabled', 'disabled');
+    stopBtn.removeAttribute('disabled');
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        bodyRef.style.backgroundColor = `${color}`;
+    }, 1000);    
+}
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function stopChange() {
+    stopBtn.setAttribute('disabled', 'disabled');
+    startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+}
